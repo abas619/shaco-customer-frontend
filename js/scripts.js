e65660a469b8b2f54fa6235ceabeebf8078ed043
Abas Dolati <lostangel619@gmail.com>
2024-11-17 12:49:06 +0330

    * user-profile, support, bottom-navbar, fix ui ux

--- js/scripts.js
+++ js/scripts.js
@@ -3,26 +3,17 @@ if (document.getElementById("sidebarCollapse") != null) {
   var Sidebar = document.getElementById("sidebar");
   var Content = document.getElementById("content");
   var adminMenuDrop = document.getElementById("admin-drop-list");
-  var siderbarOverlay = document.querySelector(".siderbar-overlay");
   var btn_closeSiderbar = document.querySelector(".btn-close-sidebar");
 
   sidebarCollapse.addEventListener("click", function () {
     Sidebar.classList.toggle("active");
     Content.classList.toggle("active");
     this.classList.toggle("active");
-    siderbarOverlay.classList.add("active");
-  });
-  siderbarOverlay.addEventListener("click", function () {
-    Sidebar.classList.remove("active");
-    Content.classList.remove("active");
-    this.classList.remove("active");
-    siderbarOverlay.classList.remove("active");
   });
   btn_closeSiderbar.addEventListener("click", function () {
     Sidebar.classList.remove("active");
     Content.classList.remove("active");
     this.classList.remove("active");
-    siderbarOverlay.classList.remove("active");
   });
 }
 // Dropdowns
@@ -82,36 +73,16 @@ if (document.querySelector("[data-price]")) {
   }
 }
 
-// Show Side Basket
-if (document.querySelector(".btn-basket") !== null) {
-  var btnsSideBasket = document.querySelectorAll(".btn-basket");
-  var sideBasket = document.getElementById("side-basket");
-  var dismissBasket = document.getElementById("dismiss-basket");
-  var basketOverlay = document.querySelector(".basket-overlay");
-  var body = document.querySelector("body");
-
-  btnsSideBasket.forEach((btnBasket) => {
-    btnBasket.addEventListener("click", function () {
-      sideBasket.classList.add("active");
-      basketOverlay.classList.add("active");
-      body.classList.add("fixedposition");
-    });
-  });
-  basketOverlay.addEventListener("click", function () {
-    sideBasket.classList.remove("active");
-    basketOverlay.classList.remove("active");
-    body.classList.remove("fixedposition");
-  });
-  dismissBasket.addEventListener("click", function () {
-    sideBasket.classList.remove("active");
-    basketOverlay.classList.remove("active");
-    body.classList.remove("fixedposition");
-  });
-}
-
-// For Delete Item from side Basket
-function deleteBasktet_item(el) {
-  el.parentElement.parentElement.parentElement.parentElement.remove();
+// جدا کردن 3 رقم
+function separate(Number) {
+  Number += "";
+  Number = Number.replace(",", "");
+  x = Number.split(".");
+  y = x[0];
+  z = x.length > 1 ? "." + x[1] : "";
+  var rgx = /(\d+)(\d{3})/;
+  while (rgx.test(y)) y = y.replace(rgx, "$1" + "," + "$2");
+  return y + z;
 }
 
 // Login
@@ -160,8 +131,8 @@ if (document.getElementById("list-payment") != null) {
       // محتوای HTML مربوط به آیتم جدید
       paymentItem.innerHTML = `
           <div class="input-group basis-2/5 m-0">
-              <input type="number" class="input !sm:text-md !text-sm"
-                  placeholder="به تومان وارد کنید">
+              <input type="text" class="input !sm:text-md !text-sm"
+                  placeholder="به تومان وارد کنید" onkeyup="javascript:this.value=separate(this.value)">
           </div>
           <div class="input-group basis-2/5 m-0">
               <input type="text" class="input !sm:text-md !text-sm"
